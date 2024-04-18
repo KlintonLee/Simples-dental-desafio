d21540d8bdb0f754c1079020cd9ff5be7b711024
klinton lee <klintonlee92@gmail.com>
2024-04-18 15:42:33 -0300

    chore: initial structure with spring web, lombok and postgres driver dependencies

---
+++ src/main/java/com/simples/dental/professionals/ProfessionalsApplication.java
@@ -0,0 +1,13 @@
+package com.simples.dental.professionals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simples/dental/professionals/ProfessionalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simples.dental.professionals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
