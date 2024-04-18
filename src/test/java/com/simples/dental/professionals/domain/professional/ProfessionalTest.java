efdca6fd0608ce41ee476fb3bf85d5a19dba775a
klinton lee <klintonlee92@gmail.com>
2024-04-18 16:28:57 -0300

    refactor: adjust test name and fix static variable to UpperCase that creates a new Profissional at domain layer

--- src/test/java/com/simples/dental/professionals/domain/professional/ProfessionalTest.java
+++ src/test/java/com/simples/dental/professionals/domain/professional/ProfessionalTest.java
@@ -11,22 +11,23 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ProfessionalTest {
 
-    private static String nome = "John Doe";
+    private static String EXPECTED_NOME = "John Doe";
 
-    private static CargoProfissional cargo = CargoProfissional.DESENVOLVEDOR;
+    private static CargoProfissional EXPECTED_CARGO = CargoProfissional.DESENVOLVEDOR;
 
-    private static Instant nascimento = LocalDate.of(1900, 12, 25).atStartOfDay(ZoneId.systemDefault()).toInstant();
+    private static Instant EXPECTED_NASCIMENTO = LocalDate.of(1900, 12, 25).atStartOfDay(ZoneId.systemDefault()).toInstant();
 
     @Test
-    public void givenAValidParams_whenCallCreateProfessional_thenReturnsANewProfessional() {
-        final var professional = Profissional.newProfissional(nome, cargo, nascimento);
+    public void givenAValidParams_whenCallNewProfissional_thenReturnsANewOne() {
+        final var professional = Profissional.newProfissional(EXPECTED_NOME, EXPECTED_CARGO, EXPECTED_NASCIMENTO);
+        final var expectedActive = true;
 
         assertNotNull(professional);
         assertNotNull(professional.getId());
-        assertEquals(nome, professional.getNome());
-        assertEquals(cargo, professional.getCargo());
-        assertEquals(nascimento, professional.getNascimento());
-        assertEquals(true, professional.isActive());
+        assertEquals(EXPECTED_NOME, professional.getNome());
+        assertEquals(EXPECTED_CARGO, professional.getCargo());
+        assertEquals(EXPECTED_NASCIMENTO, professional.getNascimento());
+        assertEquals(expectedActive, professional.isActive());
         assertNotNull(professional.getCreatedDate());
     }
 }
