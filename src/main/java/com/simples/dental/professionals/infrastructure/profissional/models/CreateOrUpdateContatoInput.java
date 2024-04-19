136925a98e8eba08445140cab2e67dd3e85f8dd8
klinton lee <klintonlee92@gmail.com>
2024-04-19 18:24:08 -0300

    chore: change create contato input to be use for update too

--- src/main/java/com/simples/dental/professionals/infrastructure/profissional/models/CreateOrUpdateContatoInput.java
+++ src/main/java/com/simples/dental/professionals/infrastructure/profissional/models/CreateOrUpdateContatoInput.java
@@ -1,15 +1,15 @@
 package com.simples.dental.professionals.infrastructure.profissional.models;
 
-public record CreateContatoInput(
+public record CreateOrUpdateContatoInput(
         String nome,
         String contato,
         String profissional_id
 ) {
-    public static CreateContatoInput with(
+    public static CreateOrUpdateContatoInput with(
             final String nome,
             final String contato,
             final String profissional_id
     ) {
-      return new CreateContatoInput(nome, contato, profissional_id);
+      return new CreateOrUpdateContatoInput(nome, contato, profissional_id);
     }
 }
