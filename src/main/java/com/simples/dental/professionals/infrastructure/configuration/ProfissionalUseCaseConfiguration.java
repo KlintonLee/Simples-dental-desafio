e9dc018c80f98204af5e26d540f46a53f280530e
klinton lee <klintonlee92@gmail.com>
2024-04-19 04:32:33 -0300

    enhancement: add beans of Profissional use cases

---
+++ src/main/java/com/simples/dental/professionals/infrastructure/configuration/ProfissionalUseCaseConfiguration.java
@@ -0,0 +1,46 @@
+package com.simples.dental.professionals.infrastructure.configuration;
+
+import com.simples.dental.professionals.application.profissional.create.CreateProfissionalUseCase;
+import com.simples.dental.professionals.application.profissional.create.DefaultCreateProfissionalUseCase;
+import com.simples.dental.professionals.application.profissional.delete.DefaultDeleteProfissionalUseCase;
+import com.simples.dental.professionals.application.profissional.delete.DeleteProfissionalUseCase;
+import com.simples.dental.professionals.application.profissional.retrieve.get.DefaultGetProfissionalByIdUseCase;
+import com.simples.dental.professionals.application.profissional.retrieve.get.GetProfissionalByIdUseCase;
+import com.simples.dental.professionals.application.profissional.update.DefaultUpdateProfissionalUseCase;
+import com.simples.dental.professionals.application.profissional.update.UpdateProfissionalUseCase;
+import com.simples.dental.professionals.domain.profissional.ProfissionalGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class ProfissionalUseCaseConfiguration {
+
+    private final ProfissionalGateway profissionalGateway;
+
+
+    public ProfissionalUseCaseConfiguration(ProfissionalGateway profissionalGateway) {
+        this.profissionalGateway = Objects.requireNonNull(profissionalGateway);
+    }
+
+    @Bean
+    public CreateProfissionalUseCase createProfissionalUseCase() {
+        return new DefaultCreateProfissionalUseCase(profissionalGateway);
+    }
+
+    @Bean
+    public GetProfissionalByIdUseCase getProfissionalByIdUseCase() {
+        return new DefaultGetProfissionalByIdUseCase(profissionalGateway);
+    }
+
+    @Bean
+    public UpdateProfissionalUseCase updateProfissionalUseCase() {
+        return new DefaultUpdateProfissionalUseCase(profissionalGateway);
+    }
+
+    @Bean
+    public DeleteProfissionalUseCase deleteProfissionalUseCase() {
+        return new DefaultDeleteProfissionalUseCase(profissionalGateway);
+    }
+}
