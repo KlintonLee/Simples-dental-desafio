25f67418f5f811c712087d136ce6fd7f0fcd3cd1
klinton lee <klintonlee92@gmail.com>
2024-04-19 15:37:48 -0300

    enhancement: update contato domain test to receive profissional object instead profissional id

--- src/main/java/com/simples/dental/professionals/domain/contato/Contato.java
+++ src/main/java/com/simples/dental/professionals/domain/contato/Contato.java
@@ -1,8 +1,10 @@
 package com.simples.dental.professionals.domain.contato;
 
 import com.simples.dental.professionals.domain.AggregateRoot;
+import com.simples.dental.professionals.domain.profissional.Profissional;
 import com.simples.dental.professionals.domain.validations.ValidationHandler;
 import lombok.Getter;
+import lombok.With;
 
 import java.time.LocalDate;
 
@@ -13,7 +15,7 @@ public class Contato extends AggregateRoot<ContatoId> {
 
     private String contato;
 
-    private String profissionalId;
+    private Profissional profissional;
 
     private LocalDate createdDate;
 
@@ -21,34 +23,34 @@ public class Contato extends AggregateRoot<ContatoId> {
             final ContatoId contatoId,
             final String nome,
             final String contato,
-            final String profissionalId,
+            final Profissional profissional,
             final LocalDate createdDate
     ) {
         super(contatoId);
         this.nome = nome;
         this.contato = contato;
-        this.profissionalId = profissionalId;
+        this.profissional = profissional;
         this.createdDate = createdDate;
     }
 
     public static Contato newContato(
             String nome,
             String contato,
-            String profissionalId
+            Profissional profissional
     ) {
         final var id = ContatoId.unique();
         final var now = LocalDate.now();
-        return new Contato(id, nome, contato, profissionalId, now);
+        return new Contato(id, nome, contato, profissional, now);
     }
 
     public Contato update(
             final String nome,
             final String contato,
-            final String profissionalId
+            final Profissional profissional
     ) {
         this.nome = nome;
         this.contato = contato;
-        this.profissionalId = profissionalId;
+        this.profissional = profissional;
         return this;
     }
 
