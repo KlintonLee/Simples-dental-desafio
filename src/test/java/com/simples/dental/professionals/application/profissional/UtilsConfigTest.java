b51a4288e9aeef8ea0e2bc42ee5af59db430a80e
klinton lee <klintonlee92@gmail.com>
2024-04-18 23:56:10 -0300

    chore: create an utils config file that holds static variables for tests

---
+++ src/test/java/com/simples/dental/professionals/application/profissional/UtilsConfigTest.java
@@ -0,0 +1,15 @@
+package com.simples.dental.professionals.application.profissional;
+
+import com.simples.dental.professionals.domain.profissional.CargoProfissional;
+
+import java.time.LocalDate;
+
+public class UtilsConfigTest {
+
+    public static String EXPECTED_NOME = "John Doe";
+
+    public static CargoProfissional EXPECTED_CARGO = CargoProfissional.DESENVOLVEDOR;
+
+    public static LocalDate EXPECTED_NASCIMENTO = LocalDate.of(2000, 12, 25);
+
+}
