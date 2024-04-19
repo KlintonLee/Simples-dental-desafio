0d16339a1c518b679b99bee18cc4d79f3ec7f41f
klinton lee <klintonlee92@gmail.com>
2024-04-19 15:37:07 -0300

    test: update contato domain test to receive profissional object instead profissional id

--- src/test/java/com/simples/dental/professionals/domain/contato/ContatoTest.java
+++ src/test/java/com/simples/dental/professionals/domain/contato/ContatoTest.java
@@ -1,27 +1,28 @@
 package com.simples.dental.professionals.domain.contato;
 
+import com.simples.dental.professionals.domain.profissional.Profissional;
 import org.junit.jupiter.api.Test;
 
+import static com.simples.dental.professionals.UtilsConfigTest.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ContatoTest {
 
-    private static String EXPECTED_NOME = "fixo";
-
-    private static String EXPECTED_CONTATO = "14-999999999";
-
-    private static String EXPECTED_PROFISSIONAL_ID = "578a1fc9-e6ec-4190-ab36-05c87eeecaf7";
-
     @Test
     public void givenAValidParams_whenCallNewContato_thenReturnsANewOne() {
-        final var contato = Contato.newContato(EXPECTED_NOME, EXPECTED_CONTATO, EXPECTED_PROFISSIONAL_ID);
+        final var profissional = Profissional.newProfissional(EXPECTED_PROFISSIONAL_NOME, EXPECTED_CARGO, EXPECTED_NASCIMENTO);
+        final var contato = Contato.newContato(EXPECTED_CONTATO_NOME, EXPECTED_CONTATO, profissional);
 
         assertNotNull(contato);
         assertNotNull(contato.getId());
-        assertEquals(EXPECTED_NOME, contato.getNome());
+        assertNotNull(contato.getProfissional());
+        assertEquals(EXPECTED_CONTATO_NOME, contato.getNome());
         assertEquals(EXPECTED_CONTATO, contato.getContato());
-        assertEquals(EXPECTED_PROFISSIONAL_ID, contato.getProfissionalId());
+        assertEquals(EXPECTED_PROFISSIONAL_NOME, contato.getProfissional().getNome());
+        assertEquals(EXPECTED_CARGO, contato.getProfissional().getCargo());
+        assertEquals(EXPECTED_NASCIMENTO, contato.getProfissional().getNascimento());
         assertNotNull(contato.getCreatedDate());
+        assertNotNull(contato.getProfissional().getCreatedDate());
     }
 }
