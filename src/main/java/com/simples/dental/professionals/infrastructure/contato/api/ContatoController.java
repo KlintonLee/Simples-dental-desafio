4239e2c4c22c770133891cdab6021f929fb81698
klinton lee <klintonlee92@gmail.com>
2024-04-19 20:08:00 -0300

    enhancement: add update and delete operations to controller

--- src/main/java/com/simples/dental/professionals/infrastructure/contato/api/ContatoController.java
+++ src/main/java/com/simples/dental/professionals/infrastructure/contato/api/ContatoController.java
@@ -3,8 +3,11 @@ package com.simples.dental.professionals.infrastructure.contato.api;
 import com.simples.dental.professionals.application.contato.ContatoOutput;
 import com.simples.dental.professionals.application.contato.create.CreateContatoCommand;
 import com.simples.dental.professionals.application.contato.create.CreateContatoUseCase;
+import com.simples.dental.professionals.application.contato.delete.DeleteContatoUseCase;
 import com.simples.dental.professionals.application.contato.retrieve.get.GetContatoByIdUseCase;
-import com.simples.dental.professionals.infrastructure.profissional.models.CreateContatoInput;
+import com.simples.dental.professionals.application.contato.update.UpdateContatoCommand;
+import com.simples.dental.professionals.application.contato.update.UpdateContatoUseCase;
+import com.simples.dental.professionals.infrastructure.profissional.models.CreateOrUpdateContatoInput;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +21,24 @@ public class ContatoController implements ContatoApi {
 
     private final GetContatoByIdUseCase getContatoByIdUseCase;
 
-    public ContatoController(CreateContatoUseCase createContatoUseCase, GetContatoByIdUseCase getContatoByIdUseCase) {
+    private final UpdateContatoUseCase updateContatoUseCase;
+
+    private final DeleteContatoUseCase deleteContatoUseCase;
+
+    public ContatoController(
+            CreateContatoUseCase createContatoUseCase,
+            GetContatoByIdUseCase getContatoByIdUseCase,
+            UpdateContatoUseCase updateContatoUseCase,
+            DeleteContatoUseCase deleteContatoUseCase
+    ) {
         this.createContatoUseCase = Objects.requireNonNull(createContatoUseCase);
         this.getContatoByIdUseCase = Objects.requireNonNull(getContatoByIdUseCase);
+        this.updateContatoUseCase = Objects.requireNonNull(updateContatoUseCase);
+        this.deleteContatoUseCase = Objects.requireNonNull(deleteContatoUseCase);
     }
 
     @Override
-    public ResponseEntity<ContatoOutput> createContato(CreateContatoInput input) {
+    public ResponseEntity<ContatoOutput> createContato(CreateOrUpdateContatoInput input) {
         final var command = CreateContatoCommand.with(input.nome(), input.contato(), input.profissional_id());
         final var contato = this.createContatoUseCase.execute(command);
         return ResponseEntity.created(URI.create("/contatos" + contato.id())).body(contato);
@@ -33,5 +47,17 @@ public class ContatoController implements ContatoApi {
     @Override
     public ContatoOutput getById(String id) {
         return this.getContatoByIdUseCase.execute(id);
+    }
+
+    @Override
+    public ResponseEntity<ContatoOutput> updateContato(String id, CreateOrUpdateContatoInput input) {
+        final var command = UpdateContatoCommand.with(id, input.nome(), input.contato(), input.profissional_id());
+        return ResponseEntity.ok(this.updateContatoUseCase.execute(command));
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteContato(String id) {
+        this.deleteContatoUseCase.execute(id);
+        return ResponseEntity.noContent().build();
     }
 }
