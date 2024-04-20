609db73cea2de6f872e87ce670a0b7075293b18f
klinton lee <klintonlee92@gmail.com>
2024-04-20 12:28:36 -0300

    enhancement: add selectByFields query for contato and profissional jpa repository

--- src/main/java/com/simples/dental/professionals/infrastructure/contato/persistence/ContatoJpaRepository.java
+++ src/main/java/com/simples/dental/professionals/infrastructure/contato/persistence/ContatoJpaRepository.java
@@ -1,6 +1,25 @@
 package com.simples.dental.professionals.infrastructure.contato.persistence;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContatoJpaRepository extends JpaRepository<ContatoJpaEntity, String> {
+
+    default List<Object[]> selectByFields(EntityManager entityManager, List<String> fields, String q) {
+        String queryString = """
+                SELECT
+                  %s
+                FROM
+                  contatos c
+                WHERE
+                  c.nome LIKE :q
+                  OR c.contato LIKE :q
+                """.formatted(String.join(", ", fields));
+        Query nativeQuery = entityManager.createNativeQuery(queryString);
+        nativeQuery.setParameter("q", "%" + q + "%");
+        return nativeQuery.getResultList();
+    }
 }
--- src/main/java/com/simples/dental/professionals/infrastructure/profissional/persistence/ProfissionalJpaRepository.java
+++ src/main/java/com/simples/dental/professionals/infrastructure/profissional/persistence/ProfissionalJpaRepository.java
@@ -1,6 +1,25 @@
 package com.simples.dental.professionals.infrastructure.profissional.persistence;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProfissionalJpaRepository extends JpaRepository<ProfissionalJpaEntity, String> {
+
+    default List<Object[]> selectByFields(EntityManager entityManager, List<String> fields, String q) {
+        String queryString = """
+                SELECT
+                  %s
+                FROM
+                  profissionais c
+                WHERE
+                  c.nome LIKE :q
+                  OR c.cargo LIKE :q
+                """.formatted(String.join(", ", fields));
+        Query nativeQuery = entityManager.createNativeQuery(queryString);
+        nativeQuery.setParameter("q", "%" + q + "%");
+        return nativeQuery.getResultList();
+    }
 }
