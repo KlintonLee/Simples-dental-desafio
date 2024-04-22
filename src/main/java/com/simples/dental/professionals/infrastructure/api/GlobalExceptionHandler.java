6ec9a0f6dd44c531c6f1a3d77f6096613010a5a0
klinton lee <klintonlee92@gmail.com>
2024-04-21 22:17:51 -0300

    log: add logs in api global exceptions handler

--- src/main/java/com/simples/dental/professionals/infrastructure/api/GlobalExceptionHandler.java
+++ src/main/java/com/simples/dental/professionals/infrastructure/api/GlobalExceptionHandler.java
@@ -4,22 +4,32 @@ import com.simples.dental.professionals.exceptions.NoStackTraceException;
 import com.simples.dental.professionals.exceptions.NotFoundException;
 import com.simples.dental.professionals.exceptions.UnprocessableEntityException;
 import com.simples.dental.professionals.exceptions.UnprocessableFieldsException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = NotFoundException.class)
-    public ResponseEntity<?> handleNotFoundException(final NotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiError.from(ex));
+    @ExceptionHandler(UnprocessableEntityException.class)
+    public ResponseEntity<?> handleUnprocessableEntityException(final UnprocessableEntityException ex) {
+        log.error("GlobalExceptionHandler.java - UnprocessableEntityException - " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ApiError.from(ex));
     }
 
-    @ExceptionHandler(value = {UnprocessableFieldsException.class, UnprocessableEntityException.class})
+    @ExceptionHandler(UnprocessableFieldsException.class)
     public ResponseEntity<?> handleUnprocessableFieldsException(final UnprocessableFieldsException ex) {
+        log.error("GlobalExceptionHandler.java - UnprocessableFieldsException - " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ApiError.from(ex));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(final NotFoundException ex) {
+        log.error("GlobalExceptionHandler.java - NotFoundException - " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiError.from(ex));
     }
 
     record ApiError(String message) {
